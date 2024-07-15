72c61a977783ee35da0d7e3b347555643bce405c
Marlon.M <maiguangyang@163.com>
2024-07-15 10:05:40 +0800

    设置默认AppLocalizations

--- lib/common/extensions/localization.dart
+++ lib/common/extensions/localization.dart
@@ -6,7 +6,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_temp/l10n/gen/app_localizations.dart';
 
+import '../constant.dart';
+
 // 扩展BuildContext
 extension BuildContextExtension on BuildContext {
-  AppLocalizations get lang => AppLocalizations.of(this)!;
+  AppLocalizations get lang =>
+      AppLocalizations.of(this) ??
+      lookupAppLocalizations(const Locale(Constant.defaultLanguage));
 }
--- lib/providers/cache.dart
+++ lib/providers/cache.dart
@@ -7,21 +7,30 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../common/config.dart';
+
 class Cache {
+  // 私有构造函数
+  Cache._internal();
+
   static final Cache _instance = Cache._internal();
 
-  factory Cache() {
+  static Cache get instance {
+    init(Config.env);
     return _instance;
   }
 
-  Cache._internal();
+  // 工厂构造函数，防止误调用
+  factory Cache() => _instance;
 
-  late SharedPreferences? _prefs;
+  SharedPreferences? _prefs;
   String? _prefix;
 
   static Future<void> init(String prefix) async {
-    _instance._prefix = prefix;
-    _instance._prefs = await SharedPreferences.getInstance();
+    if (_instance._prefs == null) {
+      _instance._prefix = prefix;
+      _instance._prefs = await SharedPreferences.getInstance();
+    }
   }
 
   /// 根据不同环境获取key
@@ -32,43 +41,43 @@ class Cache {
 
   /// 存储一个字符串值
   static Future<void> setString(String key, String value) async {
-    await _instance._prefs!.setString(getKey(key), value);
+    await _instance._prefs?.setString(getKey(key), value);
   }
 
   /// 获取一个字符串值，如果不存在返回默认值
   static String? getString(String key, {String? defaultValue}) {
-    return _instance._prefs!.getString(getKey(key)) ?? defaultValue;
+    return _instance._prefs?.getString(getKey(key)) ?? defaultValue;
   }
 
   /// 存储一个整型值
   static Future<void> setInt(String key, int value) async {
-    await _instance._prefs!.setInt(getKey(key), value);
+    await _instance._prefs?.setInt(getKey(key), value);
   }
 
   /// 获取一个整型值，如果不存在返回默认值
   static int? getInt(String key, {int defaultValue = 0}) {
-    return _instance._prefs!.getInt(getKey(key)) ?? defaultValue;
+    return _instance._prefs?.getInt(getKey(key)) ?? defaultValue;
   }
 
   /// 存储一个布尔值
   static Future<void> setBool(String key, bool value) async {
-    await _instance._prefs!.setBool(getKey(key), value);
+    await _instance._prefs?.setBool(getKey(key), value);
   }
 
   /// 获取一个布尔值，如果不存在返回默认值
   static bool? getBool(String key, {bool defaultValue = false}) {
-    return _instance._prefs!.getBool(getKey(key)) ?? defaultValue;
+    return _instance._prefs?.getBool(getKey(key)) ?? defaultValue;
   }
 
   /// 存储一个Map
   static Future<void> setMap(String key, Map<String, dynamic> value) async {
     String jsonString = jsonEncode(value);
-    await _instance._prefs!.setString(getKey(key), jsonString);
+    await _instance._prefs?.setString(getKey(key), jsonString);
   }
 
   /// 获取一个Map，如果不存在返回空Map
   static Map<String, dynamic>? getMap(String key) {
-    String? jsonString = _instance._prefs!.getString(getKey(key));
+    String? jsonString = _instance._prefs?.getString(getKey(key));
     if (jsonString != null) {
       return jsonDecode(jsonString);
     } else {
@@ -78,11 +87,11 @@ class Cache {
 
   /// 删除指定键的值
   static Future<void> remove(String key) async {
-    await _instance._prefs!.remove(getKey(key));
+    await _instance._prefs?.remove(getKey(key));
   }
 
   /// 清空所有存储的值
   static Future<void> clear() async {
-    await _instance._prefs!.clear();
+    await _instance._prefs?.clear();
   }
 }
